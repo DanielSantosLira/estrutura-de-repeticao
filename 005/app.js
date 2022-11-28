bf963265d5872b9a939e1cf1792b12c3c43f8c9e
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2022-11-28 12:31:41 -0300

    Update app.js

--- 005/app.js
+++ 005/app.js
@@ -1,19 +1,23 @@
 /*5. Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
-      A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
-      A mensagem "Reprovado", se a média for menor do que sete;
-      A mensagem "Aprovado com Distinção", se a média for igual a dez.*/
-function media(){
+      A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+      A mensagem "Reprovado", se a média for menor do que sete;
+      A mensagem "Aprovado com Distinção", se a média for igual a dez.*/
+function media() {
     let nota1 = parseFloat(document.getElementById("nota1").value);
     let nota2 = parseFloat(document.getElementById("nota2").value);
 
-    let media = (nota1 + nota2)/2 ;
 
-    if(media >= 7)
-     if(media==10)
-      alert("Uau! Aprovado com distin��o");
-     else
-      alert("Parabens, aprovado! Media "+media);
-    else
-     alert("Reprovado!")
+    let media = (nota1 + nota2) / 2;
 
-   }
+    if (media >= 7) {
+        document.getElementById("resultado").innerHTML = "Uau! Aprovado com lovor"
+        if(media == 6){
+          document.getElementById("resultado").innerHTML = "Parabens, aprovado! Media"
+        }
+    } else {
+        document.getElementById("resultado").innerHTML = "Reprovado "
+
+    }
+    document.getElementById("nota1").value = ""
+    document.getElementById("nota2").value = ""
+}

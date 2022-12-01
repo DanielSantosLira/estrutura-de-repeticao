ec7e595844ad6345bece43c43f3153c23b77fef8
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2022-11-30 22:18:52 -0300

    Update app.js

--- 009/app.js
+++ 009/app.js
@@ -1,32 +1,32 @@
 /* Faça um Programa que leia três números e mostre-os em ordem decrescente. */
 
 function exibir(){
-    var num1 = parseFloat(document.getElementById("numero1").value);
-    var num2 = parseFloat(document.getElementById("numero2").value);
-    var num3= parseFloat(document.getElementById("numero3").value);
+    var num1 = parseFloat(document.getElementById("numero1").value)
+    var num2 = parseFloat(document.getElementById("numero2").value)
+    var num3= parseFloat(document.getElementById("numero3").value)
 
-    var aux = num1;
-    var aux = num2;
-    var aux = num3;
+    var aux = num1
+    var aux = num2
+    var aux = num3
 
     if(num2 > num1){
-     aux=num2;
-     num2=num1;
-     num1=aux;
+     aux=num2
+     num2=num1
+     num1=aux
     }
 
     if(num3 > num1){
-     aux=num3;
-     num3=num1;
-     num1=aux;
+     aux=num3
+     num3=num1
+     num1=aux
     }
 
     if(num3 > num2){
-     aux=num3;
-     num3=num2;
-     num2=aux;
+     aux=num3
+     num3=num2
+     num2=aux
     }else{
         document.getElementById("resultado").innerHTML = num1+"-"+num2+"-"+num3
     }
-    document.getElementById("resultado").value = "-"
+    document.getElementById("resultado").value = ""
    }
